2ce7f2d96e33c04604c667a161d6ac6a09a36f6a
Mizu Nishikawa-Toomey <ms19mnt@tr02.tcs.cs.ox.ac.uk>
2019-11-05 23:17:15 +0000

    ready for parallel and seq

--- Primes.scala
+++ Primes.scala
@@ -4,7 +4,7 @@ object Sieve{
 
   def main(args: Array[String]) = {
     assert(args.length == 1, "must have one argument")
-    val t0 = java.lang.System.currentTimeMillis()
+
 
     val N = args(0).toInt // number of primes required
     // array will hold values with type Int and will be len N
@@ -14,29 +14,36 @@ object Sieve{
     var nextSlot = 1 // next free slot in primes
     var next = 3 // next candidate prime to consider
     
-    while(nextSlot<N)
-    {
-      // Test if next is prime; 
-      // invariant: next is coprime with primes[0..i) && p = primes(i)
-      var i = 0; var p = primes(i)
-      // while loop to go through primes list and see if next is prime.
-      // p is basically going through all the list of primes weve already accumilated
-      // while next is equal to, or larger than p**2, then choose the next larger prime
-      // if 
+    def sequential(nextSlotIn:Int) = {
+      val t0 = java.lang.System.currentTimeMillis()
+      print(t0)
+      var nextSlot = nextSlotIn
+      while(nextSlot<N)
+      {
+        // Test if next is prime;
+        // invariant: next is coprime with primes[0..i) && p = primes(i)
+        var i = 0; var p = primes(i)
+        // while loop to go through primes list and see if next is prime.
+        // p is basically going through all the list of primes weve already accumilated
+        // while next is equal to, or larger than p**2, then choose the next larger prime
+        // if
 
-      while(p*p<=next && next%p != 0){ i += 1; p = primes(i) }
-      if(p*p>next)
-      { // next is prime
-        // add element to primes array and start again at var i = 0, primes (i) = 2
-        // but this time with a new value of next incremented by 1
-        primes(nextSlot) = next; nextSlot += 1
-        // println(next)
+        while(p*p<=next && next%p != 0){ i += 1; p = primes(i) }
+        if(p*p>next)
+        { // next is prime
+          // add element to primes array and start again at var i = 0, primes (i) = 2
+          // but this time with a new value of next incremented by 1
+          primes(nextSlot) = next; nextSlot += 1
+          // println(next)
+        }
+        next += 1
       }
-      next += 1
+      // print the last prime:
+      println(primes(N-1))
+      print(java.lang.System.currentTimeMillis())
+      println("Time taken: "+(java.lang.System.currentTimeMillis()-t0))
+      // About 2.7 secs for N = 1,000,000; answer: 15,485,863
     }
-    // print the last prime:
-    println(primes(N-1))
-    println("Time taken: "+(java.lang.System.currentTimeMillis()-t0))
-    // About 2.7 secs for N = 1,000,000; answer: 15,485,863
+    sequential(nextSlot)
   }
 }
