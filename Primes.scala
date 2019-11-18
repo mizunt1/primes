3c6f7774bbd324b9d01134f526757ac7953c34dc
Mizu Nishikawa-Toomey <ms19mnt@msc03.tcs.cs.ox.ac.uk>
2019-11-18 19:03:40 +0000

    something works

--- Primes.scala
+++ Primes.scala
@@ -60,6 +60,7 @@ object Sieve{
       println("start work0")
       def worker(){
         println("start work")
+        working_on.set((Thread.currentThread().getId().toInt%num_threads), next.get())
         while(primes_filled.get() < num_primes){
           println("inwhile")
           var i = 0;
@@ -83,18 +84,23 @@ object Sieve{
           println("state of primes", primes.get(0), primes.get(1), primes.get(2))
           while(p*p <=next.get() && next.get()%p != 0){i += 1; println("what is next", next.get()); println("what is p", primes.get(i));println("what is i", i); p = primes.get(i)};
           println("inwhile3")
-          var k = 0
+          var k = 1
           println("p**2", p*p)
           println("next get", next.get())
           if (p*p>next.get()){
             // fill the primes array
-            var to_save = next.get()
-            while(k < primes_filled.get()+1){
-              println("inwhile4")
-              println("primes.get(k)", primes.get(k))
-              if(primes.get(k) > to_save){to_save = primes.getAndSet(k, to_save); k+=1}
-              println("k in while 4 ", k)
+            var saved = next.get()
+            while(k <= primes_filled.get()){
+              println("******inwhile4******")
+              print("what is k", k)
+              print("what is state of primes", primes.get(9))
+              if(k==num_primes){println("hello")}
+              else if(primes.get(k) > next.get()|| primes.get(k)==0){saved = primes.getAndSet(k, saved)}
+              println("what is k ", k)
+              println("what is next", next.get())
+              println("primes 0", primes.get(0))
               println("primes_filled", primes_filled.get())
+              println("state of primes while filling", primes.get(0), primes.get(1), primes.get(2))
               // to save is the number currently not in the array which
               // will be placed in the array at the next iteration
               // when the prime is placed in the right place, increment k by extra one,
@@ -103,15 +109,21 @@ object Sieve{
               k += 1
 
             }
+                          println("state of primes after filling", primes.get(0), primes.get(1), primes.get(2))
                         primes_filled.getAndIncrement()
           }
           next.getAndIncrement();
-          
+
         }
+        var j = 0
+        while(j < num_primes){println(primes.get(j)); j+=1}
+        
       }
       ox.cads.util.ThreadUtil.runSystem(num_workers, worker)
     }
        
     concurrent(N)
+  //  var j = 0
+//
   }
 }
